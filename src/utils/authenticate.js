d83e548df2ed391ede7c93f8969d315320175f02
Pablo <pablotachella95@gmail.com>
2022-02-24 01:10:27 -0300

    Se modifico la funcion authenticateData, reemplazando la peticion a la url del challenge por una promesa que valida las credenciales de forma local
    Esto se hizo para evitar los bloqueos por carga de contenido activo mixto (peticiones http sin cifrar)

--- src/utils/authenticate.js
+++ src/utils/authenticate.js
@@ -3,23 +3,25 @@ import axios from "axios";
 const AUTHENTICATION_URL = 'http://challenge-react.alkemy.org/'
 
 export function authenticateData(email, password) {
-  return axios
-    .post(
-      AUTHENTICATION_URL,
-      { email: email, password: password }
-    )
+  return new Promise((resolve, reject) => {
+    // Simulo la respuesta del servidor del challenge
+    if(email == 'challenge@alkemy.org' && password == 'react') {
+      resolve({data: {token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJjaGFsbGVuZ2VAYWxrZW15Lm9yZyIsImlhdCI6MTUxNjIzOTAyMn0.ilhFPrG0y7olRHifbjvcMOlH7q2YwlegT0f4aSbryBE"}})
+    } else {
+      reject('Invalid credentials')
+    }
+  })
 }
 
-// export async function authenticateData(email, password) {
-//   try {
-//     const token = await axios
-//       .post(
-//         AUTHENTICATION_URL,
-//         { email: email, password: password }
-//       ).then(res => res.data.token)
-//     return token;
-//   } catch (error) {
-//     new Error(error)
-//     return ''
-//   }
+// Reemplazar autenticateData por la siguiente funcion para una autenticacion en la url del challenge
+// Esto solo funcionará en el entorno local debido a que una vez desplegado el sitio, los navegadores
+// bloquearan el contenido obtenido mediante HTTP sin cifrar
+// error: Se ha bloqueado la carga del contenido activo mixto "http://challenge-react.alkemy.org/"
+
+// export function authenticateData(email, password) {
+//   return axios
+//     .post(
+//       AUTHENTICATION_URL,
+//       { email: email, password: password }
+//     )
 // }
